c62f328f717f0f8b954185e73fe508ed368cf486
Karolina Hetmanska <khetmanska30@wp.pl>
2016-12-01 22:10:25 +0100

    FDESD-44/ WIP, every navbar li highlighlit

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,38 @@
 /**
  * Created by khetmanska on 30.11.16.
  */
-$(document).on('scroll resize', function() {
-  var scrollFromTop = $(document).scrollTop();
-  var distanceFromTopTwo = $('#two').offset().top;
+$(document).on('scroll resize', function () {
+    var scrollFromTop = $(document).scrollTop();
 
-  console.log(scrollFromTop);
-  console.log(distanceFromTopTwo);
-  if (scrollFromTop>distanceFromTopTwo) {
-    $('#navbar-function').addClass('menu-highlight');
+    var distanceFromTopFunction = $('#two').offset().top;
+    var distanceFromTopTeam = $('#three').offset().top;
+    var distanceFromTopContact = $('#four').offset().top;
+    var distanceFromTopFooter = $('#footer').offset().top;
+
+    if (scrollFromTop >= distanceFromTopFunction
+      && scrollFromTop<distanceFromTopTeam) {
+      $('#navbar-function').addClass('menu-highlight');
+    }
+    else {
+      $('#navbar-function').removeClass('menu-highlight');
+    }
+
+    if (scrollFromTop >= distanceFromTopTeam
+      && scrollFromTop<distanceFromTopContact) {
+      $('#navbar-team').addClass('menu-highlight');
+    }
+    else {
+    $('#navbar-team').removeClass('menu-highlight');
+
   }
+    if (scrollFromTop >= distanceFromTopContact
+      && scrollFromTop<distanceFromTopFooter) {
+      $('#navbar-contact').addClass('menu-highlight');
+    }
+    else {
+      $('#navbar-contact').removeClass('menu-highlight');
 
-});
+    }
+  }
+);
+
