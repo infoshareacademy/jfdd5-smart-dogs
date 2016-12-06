727745b955e123841d57516cad5e314c5b036f4e
KubaTuscher <Kuba Tuscher>
2016-12-06 16:56:32 +0100

    score added

--- js/game.js
+++ js/game.js
@@ -51,38 +51,51 @@ function showBoard() {                           // budowanie planszy gry
 /**
  * Created by jtuscher on 30.11.16.
  */
+var score = 0;
+function updateScore() {                          // dodawanie wyniku
+  score += 1;
+  var label = 'Score: ' + score;
+  // $('table').empty(score);
+  $('#scorebutton').text(label);
+}
  // ||||||||||||||||||||||||Uruchamianie gry |||||||||||||||||||||||||||||||||||
 $('#game-display').click(function (event) {
   event.preventDefault();
   $('#game').toggleClass('show');
+  $('#scorebutton').toggleClass('show');
   $('#form-field').toggleClass('hide');
   $('#startbutton').toggleClass('show');
   showBoard();
 
-  $('table').on('click', 'td', function () {      // kontrola gracza
-    var click = {                                 // pole w ktore klika uytkownik
-      x: parseInt($(this).attr('x')),
-      y: parseInt($(this).attr('y'))
+    $('table').on('click', 'td', function () {      // kontrola gracza
+      var click = {                                 // pole w ktore klika uytkownik
+        x: parseInt($(this).attr('x')),
+        y: parseInt($(this).attr('y'))
 
-    };
+      };
 
-    var $playerCell = $('td.player');            // aktualna komorka gracza
-    console.log($playerCell);
+      var $playerCell = $('td.player');            // aktualna komorka gracza
+      console.log($playerCell);
 
-    var player = {                               // przemienia atrybuty x i y komorki tabeli na obiekt z wartosciami liczbowymi
-      x: parseInt($playerCell.attr('x')),
-      y: parseInt($playerCell.attr('y'))
-    };
+      var player = {                               // przemienia atrybuty x i y komorki tabeli na obiekt z wartosciami liczbowymi
+        x: parseInt($playerCell.attr('x')),
+        y: parseInt($playerCell.attr('y'))
+      };
 
-    console.log(click, player);
+      console.log(click, player);
 
-    if (
-      Math.abs(click.x - player.x) <= 1 &&
-      Math.abs(click.y - player.y) <= 1
-    ) {
-      $playerCell.removeClass('player');
-      $(this).addClass('player');
-    }
+      if (
+        Math.abs(click.x - player.x) <= 1 &&
+        Math.abs(click.y - player.y) <= 1
+      ) {
+        $playerCell.removeClass('player');
+        $(this).addClass('player');
+      }
+
+      if ($('td.player').hasClass('event')) {      // zdarzenie dodania punktu
+        updateScore();
+        $(this).removeClass('event');
+      }
 
     })
 });
@@ -91,7 +104,24 @@ $('#startbutton').click(function () {
   var playerCell = randomCell();
   $('.cell').removeClass('player');
   playerCell.addClass('player');
+
+  var eventCell = randomCell();
+  eventCell.addClass('event');
+
+  var intervalId = setInterval(function () {
+    var eventCell = randomCell();
+    eventCell.addClass('event');
+  }, 2000);
+
+  setTimeout(function(){
+    clearInterval(intervalId);
+    $('.cell').removeClass('player');
+  }, 6000);
+
 });
+
+
+
 
 
 
